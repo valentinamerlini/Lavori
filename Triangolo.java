b346abc87c18d62050b4beb713d420f888275bdb
valentinamerlini <valentinamerlini13-14@hotmail.com>
2017-01-08 16:57:56 +0100

    Add files via upload

---
+++ Triangolo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triangolo;
+
+/**
+ *
+ * @author Valentina
+ */
+public class Triangolo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int l1=19;
+        int l2=39;
+        int l3=24;
+        int perimetro;
+        
+        perimetro= l1 + l2 + l3;
+        System.out.println("Il perimetro del triangolo è: " +perimetro);
+        
+        
+    }
+    
+}
